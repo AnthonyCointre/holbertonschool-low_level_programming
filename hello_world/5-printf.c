0317267e8393cf8c5dc740d868462dda1887067f
AnthonyCointre <8732@holbertonstudents.com>
2024-02-19 13:24:02 +0000

    creation 5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-int main() {
-   // printf() displays the string inside quotation
-   printf("with proper grammar, but the outcome is a piece of art,");
-   return 0;
+/**
+ * main - texto
+ *
+ * Return: text in printf
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
