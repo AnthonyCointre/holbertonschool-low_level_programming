7987bc51b2100f896561e221f3458a31ef1ff7a2
AnthonyCointre <8732@holbertonstudents.com>
2024-02-20 15:29:20 +0100

    creation 1-last_digit.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -2,10 +2,12 @@
 #include <time.h>
 #include <stdio.h>
 
-/*
-	Attribuer un nombre aléatoire à la variable n à chaque fois qu'il sera exécuté.
-	Imprimer le dernier chiffre du numéro stocké dans la variable n.
-*/
+/**
+ * main - Prints the last digit of a randomly generated number
+ *        and whether it is greater than 5, less than 6, or 0.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	int n;
