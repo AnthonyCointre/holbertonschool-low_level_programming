f23fd71935a2b29d6ccccd7688b6839386921def
AnthonyCointre <8732@holbertonstudents.com>
2024-02-21 13:48:25 +0100

    creation 2-print_alphabet_x10.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - a
+ * Return:
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	for (i = 0; i < 40; i++)
+	{
+		char c = 'a';
+
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+	}
+
+	_putchar('\n');
+}
