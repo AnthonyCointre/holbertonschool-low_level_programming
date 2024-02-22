2209c7e50b425615fc9b9037303157f31171b5b3
AnthonyCointre <8732@holbertonstudents.com>
2024-02-22 17:56:37 +0100

    creation 101-quote.c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,15 +1,9 @@
-/*
- * File: 101-quote.c
- * Auth: Brennan D Baraban
- */
-
 #include <unistd.h>
 
 /**
- * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- *                followed by a new line, to standard error.
+ * main - a
  *
- * Return: Always 1.
+ * Return: a
  */
 int main(void)
 {
