67cdfad4842b8f36b215337712aa644b4aaeab96
AnthonyCointre <8732@holbertonstudents.com>
2024-02-26 17:23:07 +0100

    creation 10-print_triangle.c

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -2,10 +2,34 @@
 
 /**
  * print_triangle - a
+ * @size: a
  *
  * Return: a
  */
 void print_triangle(int size)
 {
+	int a, b, c;
 
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+
+	else
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = size - a; b > 1; b--)
+			{
+				_putchar(32);
+			}
+
+			for (c = 0; c <= a; c++)
+			{
+				_putchar(35);
+			}
+
+		_putchar('\n');
+		}
+	}
 }
