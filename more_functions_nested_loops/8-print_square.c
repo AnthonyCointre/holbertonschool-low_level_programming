c276b7040e16df84847c7819673b3f41a71f623b
AnthonyCointre <8732@holbertonstudents.com>
2024-02-26 17:22:26 +0100

    creation 8-print_square.c

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -2,10 +2,29 @@
 
 /**
  * print_square - a
+ * @size: a
  *
  * Return: a
  */
 void print_square(int size)
 {
+	int a, b;
 
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+
+	else
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+			{
+			_putchar(35);
+			}
+
+		_putchar('\n');
+		}
+	}
 }
