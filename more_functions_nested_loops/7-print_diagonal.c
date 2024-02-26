da24d3dd1cd9fd432c93790707d256722c99277c
AnthonyCointre <8732@holbertonstudents.com>
2024-02-26 15:16:42 +0100

    creation 7-print_diagonal.c

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_diagonal - a
+ * @n: a
+ * Return: a
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(92);
+	}
+
+	_putchar('\n');
+}
