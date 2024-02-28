fe21e92be7a271c72df18d56ad1b715f14918189
AnthonyCointre <8732@holbertonstudents.com>
2024-02-28 11:31:51 +0100

    creation 8-print_array.c

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_array - a
+ * @a: a
+ * @n: a
+ *
+ * Return: a
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		if (i != n - 1)
+		{
+			printf("%d, ", a[i]);
+		}
+
+		else
+		{
+			printf("%d", a[i]);
+		}
+
+		i++;
+	}
+
+	putchar('\n');
+}
