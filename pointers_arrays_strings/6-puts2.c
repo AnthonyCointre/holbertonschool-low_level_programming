3a20bcdbd3387f6e359079dac9ba73b3b1d07f1b
AnthonyCointre <8732@holbertonstudents.com>
2024-02-28 09:53:57 +0100

    creation 6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - a
+ * @str: a
+ *
+ * Return: a
+*/
+void puts2(char *str)
+{
+	int a = 0;
+
+	while (a >= 0)
+	{
+		if (str[a] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		if (a % 2 == 0)
+			_putchar(str[a]);
+		a++;
+	}
+}
