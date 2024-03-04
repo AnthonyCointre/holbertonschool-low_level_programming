ff9d216efb7fe58cbe9c2104d023e7f9e153fed0
AnthonyCointre <8732@holbertonstudents.com>
2024-03-04 11:52:47 +0100

    creation 0-memset.c

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -11,12 +11,12 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i = 0;
+	unsigned int len = 0;
 
-	while (i < n)
+	while (len < n)
 	{
-		s[i] = b;
-		i++;
+		s[len] = b;
+		len++;
 	}
 
 	return (s);
