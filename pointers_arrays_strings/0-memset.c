acd188d01b1543e499f1b7ba0f28bbf446ac784f
AnthonyCointre <8732@holbertonstudents.com>
2024-03-04 11:28:21 +0100

    creation 0-memset.c

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memset - a
+ * @s: pointeur de zone mémoire
+ * @b: octet constant
+ * @n: nombre d'octets
+ *
+ * Return: a
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}
