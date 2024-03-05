1640127052cc991cf25314f6eb7948651100123b
AnthonyCointre <8732@holbertonstudents.com>
2024-03-05 07:58:04 +0100

    creation 1-memcpy

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memcpy - a
+ * @dest: a
+ * @src: a
+ * @n: a
+ *
+ * Return: a
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+
+	}
+
+	return (dest);
+}
