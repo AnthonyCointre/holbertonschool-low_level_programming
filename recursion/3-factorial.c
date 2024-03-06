808d0c1316a54374e28fb4b7af2754e2106dafe9
AnthonyCointre <8732@holbertonstudents.com>
2024-03-06 11:05:06 +0100

    creation 3-factorial.c

---
+++ recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * factorial - a
+ * @n: a
+ *
+ * Return: a
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
+}
