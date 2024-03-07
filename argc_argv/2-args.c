f4e25a47516b23d96f611bde2f019c7bcb6eb6e4
AnthonyCointre <8732@holbertonstudents.com>
2024-03-07 15:33:14 +0100

    creation 2-args.c

---
+++ argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - a
+ * @argc: a
+ * @argv: a
+ *
+ * Return: a
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
