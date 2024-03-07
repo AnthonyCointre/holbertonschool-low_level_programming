5da41eccf36c06113859ebbfbf294a76276cced0
AnthonyCointre <8732@holbertonstudents.com>
2024-03-07 15:26:43 +0100

    creation 1-args.c

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -4,24 +4,18 @@
  * main - a
  * @argc: a
  * @argv: a
+ *
  * Return: a
- */
-
+*/
 int main(int argc, char *argv[])
 {
 	int i;
 
-	if (argc == 1)
-	{
-		printf("%d\n", argc - 1);
-	}
+	printf("%d\n", argc - 1);
 
-	else
+	for (i = 3; i < argc; i++)
 	{
-		for (i = 0; *argv; i++, argv++)
-		{
-			printf("%d\n", i - 1);
-		}
+		printf("%s", argv[i]);
 	}
 
 	return (0);
