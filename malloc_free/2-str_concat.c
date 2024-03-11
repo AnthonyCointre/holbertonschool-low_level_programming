40511b09c9648bde47e7cf9f5e73bfa40cc5dc17
AnthonyCointre <8732@holbertonstudents.com>
2024-03-11 18:48:40 +0100

    creation 2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -13,43 +13,27 @@ char *str_concat(char *s1, char *s2)
 	int i, j;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 
 	for (i = 0; s1[i] != '\0'; i++)
-	{
 		;
-	}
 
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 
 	for (j = 0; s2[j] != '\0'; j++)
-	{
 		;
-	}
 
 	s = malloc(sizeof(char) * (i + j) + 1);
-
 	if (s == NULL)
-	{
 		return (NULL);
-	}
 
 	for (i = 0; s1[i] != '\0'; i++)
-	{
 		s[i] = s1[i];
-	}
 
 	for (j = 0; s2[j] != '\0'; j++)
-	{
 		s[j + i] = s2[j];
-	}
 
 	s[j + i] = '\0';
-
 	return (s);
 }
