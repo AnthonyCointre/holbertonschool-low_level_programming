bae0cb18376b852e7f23822e951ac23b017d4d3a
AnthonyCointre <8732@holbertonstudents.com>
2024-03-12 14:36:57 +0100

    creation 4-free_grid.c

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * free_grid - a
+ * @grid: a
+ * @height: a
+ *
+ * Return: a
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+
+	free(grid);
+}
