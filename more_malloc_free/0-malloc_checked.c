66a9ce2defb18d5a3973f30cf06db07cb97c57c5
AnthonyCointre <8732@holbertonstudents.com>
2024-03-13 09:40:36 +0100

    creation 0-malloc_checked.c

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,12 +1,20 @@
 #include "main.h"
-/**
- * malloc_checked - a
- * @b: a
- *
- * Return: a
-*/
 
+/**
+ * *malloc_checked - a
+ * @b: a
+ * Return: a
+ */
 void *malloc_checked(unsigned int b)
 {
-	
+	int *i;
+
+	i = malloc(b);
+
+	if (i == NULL)
+	{
+		exit(98);
+	}
+
+	return (i);
 }
