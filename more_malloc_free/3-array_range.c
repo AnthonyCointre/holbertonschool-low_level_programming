7765d256a92f4cbebe4259659a2a74afcb1dcc47
AnthonyCointre <8732@holbertonstudents.com>
2024-03-13 21:02:51 +0100

    creation 3-array_range.c

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -9,25 +9,16 @@
 
 int *array_range(int min, int max)
 {
-	int *s;
-	int i, size, n;
+	int len, i;
+	int *ptr;
 
 	if (min > max)
-	{
 		return (NULL);
-	}
-
-	size = max - min;
-	s = malloc((size + 1) * sizeof(int));
-	i = 0;
-	n = min;
-
-	while (n <= max)
-	{
-		s[i] = n;
-		i++;
-		n++;
-	}
-
-	return (s);
+	len = max - min + 1;
+	ptr = malloc(sizeof(int) * len);
+	if (!ptr)
+		return (NULL);
+	for (i = 0; i < len; i++)
+		ptr[i] = min++;
+	return (ptr);
 }
