af6a8018d7e0b9b37e3d921eecee757a20fbcb42
AnthonyCointre <8732@holbertonstudents.com>
2024-03-13 21:01:53 +0100

    creation 2-calloc.c

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -10,15 +10,26 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *p;
+	char *s;
 
 	if (!nmemb || !size)
+	{
 		return (NULL);
-	p = malloc(nmemb * size);
-	if (!p)
+	}
+
+	s = malloc(nmemb * size);
+
+	if (!s)
+	{
 		return (NULL);
+	}
+
 	nmemb *= size;
+
 	while (nmemb--)
-		p[nmemb] = 0;
-	return (p);
+	{
+		s[nmemb] = 0;
+	}
+
+	return (s);
 }
