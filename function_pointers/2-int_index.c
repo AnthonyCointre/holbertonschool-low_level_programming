4e8f2791830345faf939caf14bfb06b8965088d7
AnthonyCointre <8732@holbertonstudents.com>
2024-03-18 11:22:17 +0100

    creation 2-int_index.c

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - a
+ * @array: a
+ * @size: a
+ * @cmp: a
+ * Return: a
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size < 1 || array == NULL || cmp == NULL)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+		{
+			return (i);
+		}
+	}
+
+	return (-1);
+}
