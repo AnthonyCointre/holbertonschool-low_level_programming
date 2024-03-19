fc32d065e31f194f2e9b589a8b835facf625ba44
AnthonyCointre <8732@holbertonstudents.com>
2024-03-19 09:12:00 +0100

    creation function_pointers.h

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,5 +1,11 @@
+#ifndef INV_TREE_H
+#define INV_TREE_H
+
 #include <stdio.h>
 #include <stdlib.h>
+
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
