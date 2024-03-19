d2569e7e6ed32b441914a25daef51134c905bdf4
AnthonyCointre <8732@holbertonstudents.com>
2024-03-19 10:13:33 +0100

    creation 3-op_functions.c

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,11 +1,13 @@
 #include "3-calc.h"
 
 /**
- * main - a
+ * get_op_func - a
+ * @s: a
  *
  * Return: a
  */
-
+int (*get_op_func(char *s))(int, int)
+{
 	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
@@ -15,3 +17,18 @@
 		{NULL, NULL}
 	};
 	int i;
+
+	i = 0;
+
+	while (i < 5)
+	{
+		if (ops[i].op[0] == s[0])
+		{
+			return (ops[i].f);
+		}
+
+		i++;
+	}
+
+	return (NULL);
+}
--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,13 +1,71 @@
 #include "3-calc.h"
 
 /**
- * main - a
+ * op_add - a
+ * @a: a
+ * @b: a
  *
  * Return: a
  */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
 
-int op_add(int a, int b) { return a + b;}
-int op_sub(int a, int b) { return a - b;}
-int op_mul(int a, int b) { return a * b;}
-int op_div(int a, int b) { return a / b;}
-int op_mod(int a, int b) { return a % b;}
+/**
+ * op_sub - a
+ * @a: a
+ * @b: a
+ *
+ * Return: a
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - a
+ * @a: a
+ * @b: a
+ *
+ * Return: a
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - a
+ * @a: a
+ * @b: a
+ *
+ * Return: a
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod - a
+ * @a: a
+ * @b: a
+ *
+ * Return: a
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
