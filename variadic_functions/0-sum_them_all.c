f072e97386624318057e7251091aa2c87716ebc9
AnthonyCointre <8732@holbertonstudents.com>
2024-03-21 09:33:26 +0100

    creation 0-sum_them_all.c

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - a
+ * @n: a
+ * @...: a
+ *
+ * Return: a
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum;
+	va_list list;
+
+	va_start(list, n);
+
+	for (i = 0, sum = 0; i < n; i++)
+	{
+		sum += va_arg(list, int);
+	}
+
+	va_end(list);
+	return (sum);
+}
---
+++ variadic_functions/README.md
@@ -0,0 +1,7 @@
+0-sum_them_all.c = renvoyer la somme de tous ses paramètres.
+
+1-print_numbers.c = imprimer des nombres, suivis d'une nouvelle ligne.
+
+2-print_strings.c = imprimer des chaînes, suivies d'une nouvelle ligne.
+
+3-print_all.c = imprime n'importe quoi.
