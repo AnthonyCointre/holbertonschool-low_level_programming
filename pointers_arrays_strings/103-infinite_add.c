3be5b10dd31658e62e418ef06dbb007c58ddebe8
AnthonyCointre <8732@holbertonstudents.com>
2024-03-29 19:27:24 +0100

    creation README.md

--- pointers_arrays_strings/103-infinite_add.c
+++ pointers_arrays_strings/103-infinite_add.c
@@ -1,93 +1,88 @@
 #include"main.h"
-
 /**
- * add_helper - a
- * @n1: a
- * @n2: a
- * @r: a
- * @size_r: a
- * @carry: a
-*/
-char *add_helper(char *n1, char *n2, char *r, int size_r, int carry)
-{
-    int index = 0;
-
-    if (n1 == r - 1 && n2 == r - 1)
-    {
-        if (carry == 1)
-            r[index] = '1';
-        else
-            r[index] = '\0';
-        return r;
-    }
-
-    if (*n1 && *n2)
-    {
-        r[index] = *n1 - '0' + *n2 + carry;
-        carry = 0;
-        if (r[index] > '9')
-        {
-            carry = 1;
-            r[index] -= 10;
-        }
-        index++;
-        n1--;
-        n2--;
-    }
-    else if (*n1)
-    {
-        r[index] = *n1 + carry;
-        carry = 0;
-        if (r[index] > '9')
-        {
-            carry = 1;
-            r[index] -= 10;
-        }
-        index++;
-        n1--;
-    }
-    else if (*n2)
-    {
-        r[index] = *n2 + carry;
-        carry = 0;
-        if (r[index] > '9')
-        {
-            carry = 1;
-            r[index] -= 10;
-        }
-        index++;
-        n2--;
-    }
-
-    if (size_r == index && (*n1 || *n2 || carry == 1))
-        return 0;
-
-    return (add_helper);
-}
-
-/**
-* infinite_add - adds two integers stored as strings
-*
-* @n1: first integer string to add
-* @n2: second integer string to add
-* @r: array to store resulting string in
-* @size_r: size of array r
-*
-* Return: the summed string in r. If r is too small for the result,
-* return (0);
-*/
+ * infinite_add - adds two integers stored as strings
+ *
+ * @n1: first integer string to add
+ * @n2: second integer string to add
+ * @r: array to store resulting string in
+ * @size_r: size of array r
+ *
+ * Return: the summed string in r. If r is too small for the result,
+ * return 0;
+ */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-    int carry = 0;
+	int carry = 0, index = 0, index2;
+	char *s1 = n1, *s2 = n2;
 
-    while (*n1)
-        n1++;
-    while (*n2)
-        n2++;
-    size_r--;
-    r[size_r] = '\0';
-    n1--;
-    n2--;
-
-    return (add_helper);
+	while (*s1 != 0)
+		s1++;
+	while (*s2 != 0)
+		s2++;
+	size_r--;
+	r[size_r] = 0;
+	s1--;
+	s2--;
+	while (s2 != n2 - 1 && s1 != n1 - 1)
+	{
+		r[index] = *s2 - '0' + *s1 + carry;
+		carry = 0;
+		if (r[index] > '9')
+		{
+			carry++;
+			r[index] -= 10;
+		}
+		index++;
+		s2--;
+		s1--;
+		if (size_r == index && (s1 != n1 - 1 || s2 != n2 - 1 || carry == 1))
+			return (0);
+	}
+	while (s1 != n1 - 1)
+	{
+		r[index] = *s1 + carry;
+		carry = 0;
+		if (r[index] > '9')
+		{
+			carry = 1;
+			r[index] -= 10;
+		}
+		s1--;
+		index++;
+		if (size_r == index && (s1 != n1 - 1 ||  carry == 1))
+			return (0);
+	}
+	while (s2 != n2 - 1)
+	{
+		r[index] = *s2 + carry;
+		carry = 0;
+		if (r[index] > '9')
+		{
+			carry = 1;
+			r[index] -= 10;
+		}
+		s2--;
+		index++;
+		if (size_r == index && (s2 != n2 - 1 || carry == 1))
+			return (0);
+	}
+	if (carry == 1)
+	{
+		r[index] = '1';
+		r[index + 1] = 0;
+	}
+	else
+	{
+		r[index--] = 0;
+	}
+	index2 = 0;
+	while (index2 <= index)
+	{
+		carry = r[index];
+		r[index] = r[index2];
+		r[index2] = carry;
+		index--;
+		index2++;
+	}
+	return (r);
 }
