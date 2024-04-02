74ce370780823e0f33bd5e23bffc3f645ebe72fc
AnthonyCointre <8732@holbertonstudents.com>
2024-04-02 14:21:25 +0200

    creation 100-first.c

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+* print_message - prints a text before the main function is executed
+*/
+
+void print_message(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
