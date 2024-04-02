2a10c1969910cf04a386e3b187bdb1388d81aa9d
AnthonyCointre <8732@holbertonstudents.com>
2024-04-02 11:56:31 +0200

    creation 1-list_len.c

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -2,7 +2,7 @@
 
 /**
  * list_len - returns the number of elements in a linked list_t list
- * @h: pointer to the head of the list
+ * @h: a pointer to the head of the list
  *
  * Return: the number of elements in a linked list_t list
 */
